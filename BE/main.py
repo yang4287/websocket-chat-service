0fd2c7352c15444f14e88358bcc5af5b8a7723e7
yangyuhui <yang4287@gmail.com>
2024-05-06 02:39:29 +0800

    fix(backend): 修改 api 輸出格式為 json 格式

--- BE/main.py
+++ BE/main.py
@@ -2,6 +2,7 @@ from typing import Union
 
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Query
 from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from typing import List, Dict
 
 
@@ -25,12 +26,25 @@ class ConnectionManager:
             del self.active_connections[client_id]
 
     async def broadcast(self, message: str):
+        # 計算當前聊天室人數
+        num_participants = len(self.active_connections)
         # 向所有活動的 WebSocket 連接廣播訊息。
         for connection in self.active_connections.values():
-            await connection.send_text(message)
+            await connection.send_json(
+                {"num_participants": num_participants, **message}
+            )
 
 
 app = FastAPI()
+
+# 設定 CORS，允許所有來源 (DEV用)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 允许所有域名，实际部署时应更严格
+    allow_credentials=True,
+    allow_methods=["*"],  # 允许所有方法（GET, POST等）
+    allow_headers=["*"],  # 允许所有头
+)
 
 # 儲存使用者名稱跟數量
 usernames: Dict[str, int] = {}
@@ -65,14 +79,16 @@ async def websocket_endpoint(websocket: WebSocket, client_id: str):
     # 當客戶端連接時，調用 manager.connect 來處理連接。
     await manager.connect(websocket, client_id)
     try:
-        await manager.broadcast(f"使用者 {client_id} 進入聊天室")
+        await manager.broadcast({"type": "join", "client_id": client_id})
         # 循環接收客戶端發送的訊息。
         while True:
             data = await websocket.receive_text()
             # 接收到訊息後，通過 manager.broadcast 廣播訊息到所有連接。
-            await manager.broadcast(f"使用者 {client_id} : {data}")
+            await manager.broadcast(
+                {"type": "message", "client_id": client_id, "message": data}
+            )
     except WebSocketDisconnect:
         # 如果捕獲到 WebSocketDisconnect 異常，說明客戶端斷開了連接。
         manager.disconnect(websocket, client_id)
         # 向所有連接廣播客戶端斷開連接的訊息。
-        await manager.broadcast(f"使用者 {client_id} 離開聊天室！")
+        await manager.broadcast({"type": "leave", "client_id": client_id})
